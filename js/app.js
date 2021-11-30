319ab37241cb0f1bfe4d414792f52b5eea8a565f
userAshk <blvckm8@gmail.com>
2021-11-30 14:43:25 +0300

    logo fxed

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 
 $(document).ready(function(){
     $("#diagnostic").click(function(e) {
+        // $('#diagnostic').css("background-color", "#00B43E")
         $('.diagcard').toggleClass('diagcardShow')
     });
 
@@ -14,3 +15,8 @@ $(document).ready(function(){
         $('.nerse__card').toggleClass('nerse__cardShow')
     })
 });
+// bunner changng
+// setInterval(function(){
+//     $('.banner').toggleClass('banner2')
+//     $('.banner2').toggleClass('banner3')
+// }, 1000)
