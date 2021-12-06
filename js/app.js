6ef9e6b41228dfa7e91887ad3d04e9d277bd777b
userAshk <blvckm8@gmail.com>
2021-12-06 15:16:58 +0300

    services

--- js/app.js
+++ js/app.js
@@ -2,21 +2,12 @@
 
 $(document).ready(function(){
     $("#diagnostic").click(function(e) {
-        // $('#diagnostic').css("background-color", "#00B43E")
-        $('.diagcard').toggleClass('diagcardShow')
+        $('.diagcard').toggleClass('diagcardShow');
+        $('.diag__name_hidden').toggleClass('diag__name');
     });
-
-    $('body').click(function(e){
-        $('.diagcardShow').toggleClass('diagcard')
-    })
-
 
     $('#nerseCard').click(function(e){
         $('.nerse__card').toggleClass('nerse__cardShow')
     })
 });
-// bunner changng
-// setInterval(function(){
-//     $('.banner').toggleClass('banner2')
-//     $('.banner2').toggleClass('banner3')
-// }, 1000)
+
