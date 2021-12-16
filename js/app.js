8fe56698f847297f9a28c9d50c73b6543329abf7
userAshk <blvckm8@gmail.com>
2021-12-16 13:14:13 +0300

    banners are ready

--- js/app.js
+++ js/app.js
@@ -47,21 +47,18 @@ $(document).ready(function(){
     })
 });
 
-var timer;
-var left = 0;
-
-function autoSlider(){
-    timer = setTimeout(() =>{
-        let bannerFlex = document.querySelector('#banner-flex');
-
-        left = left - 100;
-    
-        if(left == -300){
-            left = 0;
-            clearTimeout(timer)
-        }
-        bannerFlex.style.left = left + '%';
-        autoSlider();
-    }, 2000);
+var slideIndex = 0;
+showSlides();
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 2000); 
 }
-autoSlider();
